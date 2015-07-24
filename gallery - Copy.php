675e8e1702be600e6a3426717900bf4b66082bc3
Elma Gazetic <egazetic1@gmail.com>
2015-07-24 19:27:34 +0200

    kk

--- gallery - Copy.php
+++ gallery - Copy.php
@@ -68,137 +68,11 @@
     <link rel="stylesheet" href="fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
     <script type="text/javascript" src="fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
       
-
-      <style>
-* {
-	margin:0;
-	padding:0;
-	box-sizing:border-box;
-	}
-
-body {
-	font-family:"Helvetica Neue", Arial, sans-serif;
-	}
-
-.mosaicflow__column {
-	float:left;
-	}
-
-.mosaicflow__item {
-	position:relative;
-	}
-	.mosaicflow__item img {
-		display:block;
-		width:100%;
-		max-width:500px;
-		height:auto;
-		}
-	.mosaicflow__item p {
-		position:absolute;
-		bottom:0;
-		left:0;
-		width:100%;
-		margin:0;
-		padding:5px;
-		background:hsla(0,0%,0%,.5);
-		color:#fff;
-		font-size:14px;
-		text-shadow:1px 1px 1px hsla(0,0%,0%,.75);
-		opacity:0;
-		-webkit-transition: all 0.4s cubic-bezier(0.23,1,0.32,1);
-		   -moz-transition: all 0.4s cubic-bezier(0.23,1,0.32,1);
-		     -o-transition: all 0.4s cubic-bezier(0.23,1,0.32,1);
-		        transition: all 0.4s cubic-bezier(0.23,1,0.32,1);
-		}
-	.mosaicflow__item:hover p {
-		opacity:1;
-		}
-	</style>
-
-    
-<!--
-      <style type="text/css">
-    body {
-      margin: 0;
-      padding: 0;
-      background: #EEE;
-      font: 10px/13px 'Lucida Sans',sans-serif;
-    }
-    .wrap {
-      overflow: hidden;
-      margin: 10px;
-    }
-    .box {
-      float: left;
-      position: relative;
-      width: 20%;
-      padding-bottom: 20%;
-    }
-    .boxInner {
-      position: absolute;
-      left: 10px;
-      right: 10px;
-      top: 10px;
-      bottom: 10px;
-      overflow: hidden;
-    }
-    .boxInner img {
-      width: 100%;
-    }
-    .boxInner .titleBox {
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      margin-bottom: -50px;
-      background: #000;
-      background: rgba(0, 0, 0, 0.5);
-      color: #FFF;
-      padding: 10px;
-      text-align: center;
-      -webkit-transition: all 0.3s ease-out;
-      -moz-transition: all 0.3s ease-out;
-      -o-transition: all 0.3s ease-out;
-      transition: all 0.3s ease-out;
-    }
-    body.no-touch .boxInner:hover .titleBox, body.touch .boxInner.touchFocus .titleBox {
-      margin-bottom: 0;
-    }
-    @media only screen and (max-width : 480px) {
-      /* Smartphone view: 1 tile */
-      .box {
-        width: 100%;
-        padding-bottom: 100%;
-      }
-    }
-    @media only screen and (max-width : 650px) and (min-width : 481px) {
-      /* Tablet view: 2 tiles */
-      .box {
-        width: 50%;
-        padding-bottom: 50%;
-      }
-    }
-    @media only screen and (max-width : 1050px) and (min-width : 651px) {
-      /* Small desktop / ipad view: 3 tiles */
-      .box {
-        width: 33.3%;
-        padding-bottom: 33.3%;
-      }
-    }
-    @media only screen and (max-width : 1290px) and (min-width : 1051px) {
-      /* Medium desktop: 4 tiles */
-      .box {
-        width: 25%;
-        padding-bottom: 25%;
-      }
-    }
-  </style>
--->
- 
+   
   </head>
 
 
-<body class="no-touch">
+<body>
      <!-- BEGAIN PRELOADER -->
     <div id="preloader">
       <div id="status">&nbsp;</div>
@@ -249,35 +123,53 @@ body {
 
     </header>
     <!--=========== End HEADER SECTION ================--> 
-<br/><br/><br/><br/><br/>
+
+    <div class="content">
+        
+        <svg xmlns="http://www.w3.org/2000/svg" width="0px" height="0px" class="polaroid" preserveAspectRatio="xMidYMid meet" viewBox="0 0 0 0">
+					<g transform="rotate(-9.424738883972168 131.13208007812517,131.28302001953134) "><rect id="svg_6" height="194.264154" width="194.264154" y="34.15094" x="34"/><rect id="svg_7" height="116.226417" width="152.754719" y="56.566035" x="54.754717"/></g><rect id="svg_2" height="0" width="1" y="108" x="123" stroke-width="10" stroke="#000000" fill="#ffffff"/><g id="svg_10" transform="rotate(8.810273170471191 211.13208007812494,164.28302001953114) "><rect id="svg_11" height="194.264154" width="194.264154" y="67.15094" x="114"/><rect id="svg_12" height="116.226417" width="152.754719" y="89.566035" x="134.754717"/></g><g id="svg_13" transform="rotate(-14.838730812072754 350.132080078125,168.28302001953125) "><rect id="svg_14" height="194.264154" width="194.264154" y="71.15094" x="253"/><rect id="svg_15" height="116.226417" width="152.754719" y="93.566035" x="273.754717"/></g><g id="svg_16" transform="rotate(-1.4553210735321045 476.1320800781219,121.28302001953003) "><rect id="svg_17" height="194.264154" width="194.264154" y="24.15094" x="379"/><rect id="svg_18" height="116.226417" width="152.754719" y="46.566035" x="399.754717"/></g><g id="svg_19" transform="rotate(-4.776742458343506 556.1320800781244,176.28302001953122) "><rect id="svg_20" height="194.264154" width="194.264154" y="79.15094" x="459"/><rect id="svg_21" height="116.226417" width="152.754719" y="101.566035" x="479.754717"/></g><g id="svg_22" transform="rotate(17.17530059814453 690.1320800781251,163.2830200195312) "><rect id="svg_23" height="194.264154" width="194.264154" y="66.15094" x="593"/><rect id="svg_24" height="116.226417" width="152.754719" y="88.566035" x="613.754717"/></g><g id="svg_25" transform="rotate(-9.506031036376953 771.132080078125,177.28302001953094) "><rect id="svg_26" height="194.264154" width="194.264154" y="80.15094" x="674"/><rect id="svg_27" height="116.226417" width="152.754719" y="102.566035" x="694.754717"/></g><g id="svg_28" transform="rotate(19.351133346557617 862.1320800781251,137.28302001953148) "><rect id="svg_29" height="194.264154" width="194.264154" y="40.15094" x="765"/><rect id="svg_30" height="116.226417" width="152.754719" y="62.566035" x="785.754717"/></g>
+				</svg>
+        
+        <svg xmlns="http://www.w3.org/2000/svg" width="0px" height="0px" class="polaroid" preserveAspectRatio="xMidYMid meet" viewBox="0 0 0 0">
+					<!-- Created with SVG-edit - http://svg-edit.googlecode.com/ -->
+					<rect id="svg_2" height="0" width="1" y="108" x="123"/>
+					<rect id="svg_6" height="194.264154" width="194.264154" y="22.15094" x="18"/>
+					<rect id="svg_7" height="116.226417" width="152.754719" y="44.566035" x="38.754717"/>
+					<rect id="svg_1" height="194.264154" width="194.264154" y="83.584903" x="87.73585"/>
+					<rect id="svg_3" height="116.226417" width="152.754719" y="105.999998" x="108.490567"/>
+				</svg>
          <?php 
             $dirname = "images/gallery";
             $files = scandir($dirname);
             $out = array_shift($files);
             $out = array_shift($files);
 
-            $imenagalerija= array($files[0] => "galerija jedan",$files[1] => "galerija dva",$files[2] => "galerija tri",$files[3] => "galerija cetiri",$files[4] => "galerija pet",$files[5] => "galerija sest");
-            $opisgalerija= array($files[0] => "opis za galeriju jedan",$files[1] => "opis za galeriju dva",$files[2] => "opis za galeriju tri",$files[3] => "opis za galeriju cetiri",$files[4] => "opis za galeriju pet",$files[5] => "opis za galeriju sest");
+            $imenagalerija= array($files[0] => "galerija jedan",$files[1] => "galerija dva",$files[2] => "galerija tri",$files[3] => "galerija cetiri",$files[4] => "galerija pet");
+            $opisgalerija= array($files[0] => "opis za galeriju jedan",$files[1] => "opis za galeriju dva",$files[2] => "opis za galeriju tri",$files[3] => "opis za galeriju cetiri",$files[4] => "opis za galeriju pet");
         ?>
-<div class="wrap">
-       <?php $i = 0; foreach($files as $directoryname):?>
-             
-<!--
-    <div class="box">
-      <div class="boxInner">
+            <section id="photostack-2" class="photostack photostack-start">
+            <div>
+            <?php 
+            $i = 0;
+            foreach($files as $directoryname):?>
+            	<figure>
+						<a href="javascript:;" class="<?php echo "gallery".$i;?>" id="<?php echo 'gallery'.$i; ?>"><img src="<?php echo $dirname."/".$files[$i]."/tab.JPG"; ?>" alt="slika"/></a>
+						<figcaption>
+							<h2 class="photostack-title"><?php echo $imenagalerija[$directoryname];  ?></h2>
+							<div class="photostack-back">
+								<p><?php echo $opisgalerija[$directoryname]; ?></p>
+							</div>
+						</figcaption>
+					</figure>
+            <?php $i = $i + 1; endforeach;  ?>
+            </div>
+			</section>
+            <?php
+//$images = glob($dirname."*.jpg");
+//foreach($images as $image): ?>
+<!--<div class="picture" style="background-image:url('<?php echo $image ?>')" ></div>-->
+     <?php //endforeach;  ?>
         
-        <div class="titleBox"><div class="<?php echo "gallery".$i;?>"><?php echo $imenagalerija[$directoryname]; ?></div></div>
-      </div>
-    </div>
--->
-    <div class="mosaicflow" data-item-height-calculation="attribute">
-		<div class="mosaicflow__item">
-          <a href="javascript:;" ><img src="<?php echo $dirname."/".$files[$i]."/tab.JPG"; ?>"  alt=""></a>
-          
-			<p></p>
-		</div>
-    </div>
-    <?php $i = $i+1; endforeach;?>
     </div>
      <!--=========== BEGAIN FOOTER ================-->
      <footer id="footer">
@@ -330,7 +222,6 @@ body {
     <script src="js/classie.js"></script>
     <script src="js/elastic_grid.min.js"></script>
     
-    <script src="mosaic/jquery.mosaicflow.js"></script>
 
     <!-- Custom js-->
     <script src="js/custom.js"></script>
@@ -340,28 +231,14 @@ body {
             $(".fancybox").fancybox();
             });
         </script>
-    
-    
-  <script type="text/javascript">
-    $(function(){
-      // See if this is a touch device
-      if ('ontouchstart' in window)
-      {
-        // Set the correct body class
-        $('body').removeClass('no-touch').addClass('touch');
-        
-        // Add the touch toggle to show text
-        $('div.boxInner img').click(function(){
-          $(this).closest('.boxInner').toggleClass('touchFocus');
-        });
-      }
-    });
-  </script>
-    
 <script>
-    
-    
-		
+			// [].slice.call( document.querySelectorAll( '.photostack' ) ).forEach( function( el ) { new Photostack( el ); } );
+			
+			new Photostack( document.getElementById( 'photostack-2' ), {
+				callback : function( item ) {
+					//console.log(item)
+				}
+			} );
             
              $(".gallery0").click(function() {
                 $.fancybox.open([
@@ -468,32 +345,6 @@ body {
             });
     
                       $(".gallery4").click(function() {
-                $.fancybox.open([
-                    {
-                        href : 'images/gallery/gallery2/1.JPG'
-                    }, {
-                        href : 'images/gallery/gallery2/2.JPG'
-                    }, {
-                        href : 'images/gallery/gallery2/3.JPG'
-                    }, {
-                        href : 'images/gallery/gallery2/4.JPG'
-                    }, {
-                        href : 'images/gallery/gallery2/5.JPG'
-                    }, {
-                        href : 'images/gallery/gallery2/6.JPG'
-                    }
-
-                ], {
-                    helpers : {
-                        thumbs : {
-                            width: 75,
-                            height: 50
-                        }
-                    }
-                });
-            });
-    
-                          $(".gallery5").click(function() {
                 $.fancybox.open([
                     {
                         href : 'images/gallery/gallery2/1.JPG'
